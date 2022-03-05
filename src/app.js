c7a6e9c05b786022725ea653111cb66e33a0846c
nguyentrieutien1 <59260191+nguyentrieutien1@users.noreply.github.com>
2022-03-05 15:11:23 +0700

    hihi

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 const database = require("./connectDb/connect.db");
 const route = require("./routes/index");
 const cors = require("cors");
-app.use(cors());
+app.use(cors({ origin: "https://booking-client-project.herokuapp.com" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 database.connect();
